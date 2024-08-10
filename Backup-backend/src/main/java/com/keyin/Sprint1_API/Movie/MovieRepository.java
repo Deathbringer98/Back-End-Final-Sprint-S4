96712a2e13b82d5bf751ce13bd715a5b145ef246
Matt <73149745+Deathbringer98@users.noreply.github.com>
2024-08-10 20:17:12 -0230

    Add files via upload

--- Backup-backend/src/main/java/com/keyin/Sprint1_API/Movie/MovieRepository.java
+++ Backup-backend/src/main/java/com/keyin/Sprint1_API/Movie/MovieRepository.java
@@ -1,12 +1,10 @@
 package com.keyin.Sprint1_API.Movie;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-
-import java.util.List;
-
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
     List<Movie> findByTitleContainingIgnoreCase(String title);
 }
